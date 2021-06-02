93b8aaf8ddc90edd0daa90eedf965f03fb780506
Ezequiel Grcia <egarcia.informatic.ing@gmail.com>
2021-06-02 18:22:12 -0400

    adds class types function loader

--- packages/core/src/loaders.ts
+++ packages/core/src/loaders.ts
@@ -2,6 +2,17 @@ import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge'
 import { loadFilesSync } from '@graphql-tools/load-files'
 import { DocumentNode, GraphQLSchema, Source } from 'graphql'
 import { NonEmptyArray } from 'type-graphql'
+import { Model } from 'sequelize-typescript'
+import { ClassType } from 'type-graphql'
+
+export function loadClassTypeFiles<T extends Model>(
+    path: NonEmptyArray<string>
+): ClassType<T>[] {
+    return loadFilesSync<ClassType<T>>([...path], {
+        recursive: true,
+        extensions: ['ts', 'js'],
+    })
+}
 
 export function loadResolverDirectories(
     path: NonEmptyArray<string>,
